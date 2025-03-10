08666e9886ee485b65ef5760a48c8f24a5e44ba1
coligorm <colinrhysgorman@gmail.com>
2025-03-10 16:31:20 +1100

    Added mouse clicking control to MainPage.xaml.cs

--- AnimalMatchingGame/AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/AnimalMatchingGame/MainPage.xaml.cs
@@ -2,8 +2,12 @@
 {
     public partial class MainPage : ContentPage
     {
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
 
-        public MainPage()
+        public 
+            MainPage()
         {
             InitializeComponent();
         }
@@ -35,8 +39,36 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            if (sender is Button buttonClicked)
+            {
+                if (!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.BackgroundColor = Colors.DarkBlue;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text) && (!String.IsNullOrWhiteSpace(buttonClicked.Text)))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
 
+            if (matchesFound == 8)
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
         }
+
     }
 
 }
