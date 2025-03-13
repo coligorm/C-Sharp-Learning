06ea143a8a551a96a1e20b49a526f18c8391c689
coligorm <colinrhysgorman@gmail.com>
2025-03-13 12:17:05 +1100

    Craeted simple random card picker console app
    Head First C# - Chapter 3

---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number of cards to pick: ");
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int numberOfCards))
+            {
+                string[] cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in cards)
+                {
+                    Console.WriteLine($"You've picked the {card} from the deck");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
