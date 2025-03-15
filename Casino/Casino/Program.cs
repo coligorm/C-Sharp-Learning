9e6e1cd5f19e8219aa07fde4e89a1004d516e3c3
coligorm <colinrhysgorman@gmail.com>
2025-03-15 18:05:27 +1100

    Built Casino app guying Guy class
    Chapter 3 - Exercise p183

---
+++ Casino/Casino/Program.cs
@@ -0,0 +1,40 @@
+﻿using Guys;
+
+double odds = .75;
+Guy player = new Guy() { Name = "The player", Cash = 100 };
+
+Console.WriteLine($"Welcome to the casino. The odds are {odds}");
+
+while (true)
+{
+    player.WriteMyInfo();
+    Console.Write("How much do you want to bet: ");
+    string? howMuch = Console.ReadLine();
+
+    int pot = 0;
+    //double rand = 0;
+    if (int.TryParse(howMuch, out int amount))
+    {
+        pot = player.GiveCash(amount);        
+    }
+    if (pot > 0)
+    {
+        pot *= 2;
+        double rand = Random.Shared.NextDouble();
+        if (rand > odds)
+        {
+            Console.WriteLine($"You win {pot}");
+            player.ReceiveCash(pot);
+        }
+        else
+        {
+            Console.WriteLine("Bad luck, you lose.");
+        }
+    }
+
+    if (player.Cash <= 0)
+    {
+        Console.WriteLine("The house always wins.");
+        break;
+    }
+}
