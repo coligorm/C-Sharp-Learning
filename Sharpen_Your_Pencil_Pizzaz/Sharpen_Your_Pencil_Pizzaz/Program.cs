d9848e4ff0fe94e3932a13900cd7bb721ebb3d02
coligorm <colinrhysgorman@gmail.com>
2025-03-17 23:13:15 +1100

    Init program commit

---
+++ Sharpen_Your_Pencil_Pizzaz/Sharpen_Your_Pencil_Pizzaz/Program.cs
@@ -0,0 +1,42 @@
+﻿Pizzaz foxtrot = new Pizzaz() { Zippo = 2 };
+foxtrot.Bamboo(foxtrot.Zippo);
+
+Pizzaz november = new Pizzaz() { Zippo = 3 };
+Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
+
+while (tango.Lala(november.Zippo))
+{
+    november.Zippo *= -1;
+    november.Bamboo(tango.Vavavoom);
+    foxtrot.Bamboo(november.Zippo);
+    tango.Vavavoom -= foxtrot.Zippo;
+}
+
+Console.WriteLine("november.Zippo = " + november.Zippo);
+Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
+Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
+
+class Abracadabra
+{
+    public int Vavavoom;
+
+    public bool Lala(int floq)
+    {
+        if (floq < Vavavoom)
+        {
+            Vavavoom += floq;
+            return true;
+        }
+        return false;
+    }
+}
+
+class Pizzaz
+{
+    public int Zippo;
+
+    public void Bamboo(int eek)
+    {
+        Zippo += eek;
+    }
+}
