6ac57eab52e637985412e260b4f0b395e3e84127
coligorm <colinrhysgorman@gmail.com>
2025-03-17 23:28:20 +1100

    Added comments before running the code to walking through code and write answer for sharpen your pencil exercise p185

--- Sharpen_Your_Pencil_Pizzaz/Sharpen_Your_Pencil_Pizzaz/Program.cs
+++ Sharpen_Your_Pencil_Pizzaz/Sharpen_Your_Pencil_Pizzaz/Program.cs
@@ -1,20 +1,20 @@
-﻿Pizzaz foxtrot = new Pizzaz() { Zippo = 2 };
-foxtrot.Bamboo(foxtrot.Zippo);
+﻿Pizzaz foxtrot = new Pizzaz() { Zippo = 2 };            // foxtrot.Zippo = 2
+foxtrot.Bamboo(foxtrot.Zippo);                          // foxtrot.Zippo = 4 (Bamboo(2) => 2 += 2)
 
-Pizzaz november = new Pizzaz() { Zippo = 3 };
-Abracadabra tango = new Abracadabra() { Vavavoom = 4 };
+Pizzaz november = new Pizzaz() { Zippo = 3 };           // november.Zippo = 3
+Abracadabra tango = new Abracadabra() { Vavavoom = 4 }; // tango.Vavavoom = 4
 
-while (tango.Lala(november.Zippo))
+while (tango.Lala(november.Zippo)) // L1:true (tango.Lala(3) => 3 < 4 => tango.Vavavoom = 7 | L2:false (tango.Lala(4) => 4 < -1
 {
-    november.Zippo *= -1;
-    november.Bamboo(tango.Vavavoom);
-    foxtrot.Bamboo(november.Zippo);
-    tango.Vavavoom -= foxtrot.Zippo;
+    november.Zippo *= -1;               // L1: november.Zippo = -3
+    november.Bamboo(tango.Vavavoom);    // L1: november.Zippo = 4 (-3 + 7)
+    foxtrot.Bamboo(november.Zippo);     // L1: foxtrot.Zippo = 8 (4 + 4)
+    tango.Vavavoom -= foxtrot.Zippo;    // L1: tango.Vavavoom = -1 (7 - 8)
 }
 
-Console.WriteLine("november.Zippo = " + november.Zippo);
-Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);
-Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);
+Console.WriteLine("november.Zippo = " + november.Zippo);    // 4
+Console.WriteLine("foxtrot.Zippo = " + foxtrot.Zippo);      // 8
+Console.WriteLine("tango.Vavavoom = " + tango.Vavavoom);    // -1
 
 class Abracadabra
 {
