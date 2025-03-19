32b121c39ec30c7775da82fc19125fec5211ff1e
coligorm <colinrhysgorman@gmail.com>
2025-03-20 00:44:17 +1100

    WIP - Multiple player option added

--- Casino/Casino/Program.cs
+++ Casino/Casino/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Guys;
-
+//
+// OPTIONAL ADDITION
+// Add a way to change odds
+// Better odds, higher reward
+//
 double odds = .75;
-Guy player1 = new Guy() { Name = "Player 1", Cash = 100 };
-Guy player2 = new Guy() { Name = "Player 2", Cash = 100 };
 List<Guy> players = new List<Guy>();
 
 Console.WriteLine($"Welcome to the casino. The odds are {odds}");
@@ -18,62 +20,55 @@ if (int.TryParse(input, out int numberOfPlayers))
 }
 else
 {
-    Console.WriteLine($"{player2.Name}: Please enter a valid number.");
+    Console.WriteLine("Please enter a valid number of players.");
 }
-
-while (players.Any(player => player.Cash > 0)) 
+//
+// TO FIX:
+// Game doesnt exit when a player busts
+//
+while (players.Any(player => player.Cash > 0))
 {
     foreach (var player in players)
     {
+        Console.WriteLine("___________________");
+        Console.WriteLine($"{player.Name}'s turn");
         player.WriteMyInfo();
         Console.Write($"How much does {player.Name} want to bet: ");
         string? howMuch = Console.ReadLine();
-        if (int.TryParse(howMuch, out int amount))
+        if (int.TryParse(howMuch, out int amount) && (amount <= player.Cash))
         {
-            
-        }
-    }
-    player1.WriteMyInfo();
-    player2.WriteMyInfo();
-    Console.Write($"How much does {player1.Name} want to bet: ");
-    string? howMuchPlayer1 = Console.ReadLine();
-    Console.Write($"How much does {player2.Name} want to bet: ");
-    string? howMuchPlayer2 = Console.ReadLine();
-
-    if (int.TryParse(howMuchPlayer1, out int amountP1))
-    {
-        if (int.TryParse(howMuchPlayer2, out int amountP2))
-        {
-            // Double or nothing bet
-            int pot = player1.GiveCash(amountP1) * 2;
-            pot = player2.GiveCash(amountP2) * 2;
+            int bet = player.GiveCash(amount);
+            int pot = bet * 2;
             if (pot > 0)
             {
-                // Player 1 Players
                 if (Random.Shared.NextDouble() > odds)
                 {
-                    Console.WriteLine($"{player1.Name} wins {pot}");
-                    player1.ReceiveCash(pot);
-                }
-                else if (Random.Shared.NextDouble() > odds)
-                {
-                    Console.WriteLine($"{player2.Name} wins {pot}");
-                    player2.ReceiveCash(pot);
+                    Console.WriteLine($"{player.Name} wins {pot}");
+                    player.ReceiveCash(pot);
                 }
                 else
                 {
-                    Console.WriteLine("Bad luck, you both lose.");
+                    Console.WriteLine("Unlucky, you lose!");
                 }
+            }
+            else
+            {
+                Console.WriteLine("Not enough money in the pot!");
             }
         }
         else
         {
-            Console.WriteLine($"{player2.Name}: Please enter a valid number.");
+            Console.WriteLine($"{player.Name}: Please enter a valid anount to bet.");
         }
-    }
-    else
-    {
-        Console.WriteLine($"{player1.Name}: Please enter a valid number.");
-    }
+
+        if (player.Cash == 0)
+        {
+            Console.WriteLine($"{player.Name} has bust!");
+        }
+    } 
 }
-Console.WriteLine("At least one of ye got away with something!");
+//
+// OPTIONAL ADDITION:
+// Add a way to continue and remove the player who bust
+//
+Console.WriteLine("Game Over!");
