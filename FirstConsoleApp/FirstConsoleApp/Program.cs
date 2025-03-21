eb4014f4287aaaf60606ae6985eb5809a2106f0f
coligorm <colinrhysgorman@gmail.com>
2025-03-21 17:29:48 +1100

    Chapter 4 type casting practise exercises

--- FirstConsoleApp/FirstConsoleApp/Program.cs
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -236,3 +236,34 @@ void TradeMoney()
         
     }
 }
+
+/*
+Chapter 4 Console Exercise p 202
+*/
+
+float myFloatValue = 10;
+int myIntValue = (int)myFloatValue;
+Console.WriteLine("myIntValue is " + myIntValue);
+// Cannot implicitly convert type 'float' to 'int'. An explicit conversion exists (are you missing a cast?)
+
+/*
+Chapter 4 Console Exercise p 203
+Comment out type casting that isnt allowed
+*/
+int myInt = 10;
+byte myByte = (byte)myInt;
+double myDouble = (double)myByte;
+// bool myBool = (bool)myDouble;
+string myString = "false";
+// myBool = (bool)myString;
+//myString = (string)myInt;
+myString = myInt.ToString();
+//myBool = (bool)myByte;
+//myByte = (byte)myBool;
+short myShort = (short)myInt;
+char myChar = 'x';
+//myString = (string)myChar;
+long myLong = (long)myInt;
+decimal myDecimal = (decimal)myLong;
+myString = myString + myInt + myByte +
+myDouble + myChar;
