1ae750f6724fbddf285b8b8547e93334ed90ca8e
coligorm <colinrhysgorman@gmail.com>
2025-03-22 13:49:43 +1100

    Ability Score Calculator exercise p213

--- AbilityScore/AbilityScore/Program.cs
+++ AbilityScore/AbilityScore/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AbilityScore;
+
+AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+
+while (true)
+{
+    calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+    calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+    calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+    calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+    calculator.CalculateAbilityScore();
+    Console.WriteLine($"Calculated ability score: {calculator.Score}");
+    Console.WriteLine($"Press Q to quit, any other key to continue");
+    char keyChar = Console.ReadKey(true).KeyChar;
+
+    if ((keyChar == 'Q') || (keyChar == 'q'))
+    {
+        return;
+    }
+}
+
+static int ReadInt(int defaultValue, string prompt)
+{
+    Console.Write($"{prompt} [{defaultValue}]: ");
+    if (int.TryParse(Console.ReadLine(), out int newValue))
+    {
+        Console.WriteLine($"\t using value {newValue}");
+        defaultValue = newValue;
+    }
+    else
+    {
+        Console.WriteLine($"\t using default value {defaultValue}");
+    }
+    return defaultValue;
+}
+
+static double ReadDouble(double defaultValue, string prompt)
+{
+    Console.Write($"{prompt} [{defaultValue}]: ");
+    if (double.TryParse(Console.ReadLine(), out double newValue))
+    {
+        Console.WriteLine($"\t using value {newValue}");
+        defaultValue = newValue;
+    }
+    else
+    {
+        Console.WriteLine($"\t using default value {defaultValue}");
+    }
+    return defaultValue;
+}
